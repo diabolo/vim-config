c167a65888e3a23dcc36299c367c6075ad905c53
Andrew Premdas <apremdas@yahoo.co.uk>
2012-06-06 01:31:43 +0100

    Added vimwiki plugin

--- update_bundles.rb
+++ update_bundles.rb
@@ -24,6 +24,7 @@ git_bundles = [
   "http://github.com/vim-scripts/Gist.vim.git",
   "http://github.com/vim-scripts/IndexedSearch.git",
   "http://github.com/vim-scripts/jQuery.git",
+  "https://github.com/vim-scripts/vimwiki",
   "https://github.com/kchmck/vim-coffee-script.git",
 ]
 
